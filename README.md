a0878707b38926e09fd980415f28b27aa45fdd50
unknown <dozh@hw296.corp.ecc.kz>
2017-03-06 09:10:09 +0600

    until actions and reducers

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # redux-tutorial
 https://habrahabr.ru/company/mailru/blog/303456/
+
+My first redux-tutorial for voting app from the scratch.
+Uses: TDD, Immutable-js, Node+Redux for backend, React+Redux for frontend,
+ES6, Babel, Socket.io, Webpack и Mocha.
---
+++ voting-server/test/core_spec.js
@@ -0,0 +1,178 @@
+import {List, Map} from 'immutable';
+import {expect} from 'chai';
+
+import {setEntries, next, vote} from '../src/core';
+
+describe('application logic', () => {
+	
+	describe('setEntries', () => {
+		
+		it('dobavliaet zapisi k sostoianiu', () => {
+			const state = Map();
+			const entries = List.of('Trainspotting', '28 Days Later');
+			const nextState = setEntries(state, entries);
+			
+			expect(nextState).to.equal(Map({
+				entries: List.of('Trainspotting', '28 Days Later')
+			}));
+			
+		});
+		
+		it('preobrazuet v immutable', () => {
+			const state  = Map();
+			const entries = ['Trainspotting', '28 Days Later'];
+			const nextState = setEntries(state, entries);
+			
+			expect(nextState).to.equal(Map({
+				entries: List.of('Trainspotting', '28 Days Later')
+			}));
+		});
+	});
+	
+	describe('next', () => {
+		
+		it('beret dlia golosovania sleduiushie dve zapisi', () => {
+			const state = Map({
+				entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+			});
+			const nextState = next(state);
+			
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later')
+				}),
+				entries: List.of('Sunshine')
+			}));
+			
+		});
+		
+		it('pomeshaet pobeditelia tekushego golosovania v konec spiska zapisei', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 4,
+						'28 Days Later': 2
+					})
+				}),
+				entries:List.of('Sunshine', 'Millions', '127 Hours')
+				
+			});
+			const nextState = next(state);
+			
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Sunshine', 'Millions')
+				}),
+				entries: List.of('127 Hours', 'Trainspotting')
+			}));
+			
+		});
+		
+		it('v sluchae nichei pomeshaet obe zapisi v konec spiska', () => {
+			const state = Map({
+				vote:Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 3,
+						'28 Days Later': 3
+					})
+				}),
+				entries: List.of('Sunshine', 'Millions', '127 Hours')
+			});
+			const nextState = next(state);
+			
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Sunshine', 'Millions')
+				}),
+				entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+			}));
+		});
+		
+		it('kogda ostaetsa lish odna zapis, pomechaet ee kak pobeditelya', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 4,
+						'28 Days Later': 2
+					})
+				}),
+				entries: List()
+			});
+			const nextState = next(state);
+			
+			expect(nextState).to.equal(Map({
+				winner: 'Trainspotting'
+			}));			
+			
+		});
+		
+	});
+	
+	describe('vote', () => {
+		
+		it('sozdaet rezultat golosovania dlya vybrannoi zapisi', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later')
+				}),
+				entries: List()
+			});
+			const nextState = vote(state, 'Trainspotting');
+			
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting' : 1
+					})
+				}),
+				entries:List()
+			}));
+			
+		});
+		
+		it('dobavliaet v uje imeiushisya rezultat dlya vybrannoi zapisi', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 3,
+						'28 Days Later': 2
+					})
+				}),
+				entries: List()
+			});
+			const nextState = vote(state, 'Trainspotting');
+			
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 4,
+						'28 Days Later': 2
+					})
+				}),
+				entries: List()
+			}));
+			
+		});
+	});
+	
+	
+	
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
