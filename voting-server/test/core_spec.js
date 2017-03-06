4a18d36f480c024fd53bba5f85a7439fca5482be
unknown <dozh@hw296.corp.ecc.kz>
2017-03-06 17:17:08 +0600

    reducer composition

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -112,60 +112,45 @@ describe('application logic', () => {
 	});
 	
 	describe('vote', () => {
-		
 		it('sozdaet rezultat golosovania dlya vybrannoi zapisi', () => {
 			const state = Map({
-				vote: Map({
-					pair: List.of('Trainspotting', '28 Days Later')
-				}),
-				entries: List()
+				pair: List.of('Trainspotting', '28 Days Later')
 			});
 			const nextState = vote(state, 'Trainspotting');
 			
 			expect(nextState).to.equal(Map({
-				vote: Map({
-					pair: List.of('Trainspotting', '28 Days Later'),
-					tally: Map({
-						'Trainspotting' : 1
-					})
-				}),
-				entries:List()
+				pair: List.of('Trainspotting', '28 Days Later'),
+				tally: Map({
+					'Trainspotting': 1
+				})
 			}));
-			
 		});
 		
 		it('dobavliaet v uje imeiushisya rezultat dlya vybrannoi zapisi', () => {
+			
 			const state = Map({
-				vote: Map({
-					pair: List.of('Trainspotting', '28 Days Later'),
-					tally: Map({
-						'Trainspotting': 3,
-						'28 Days Later': 2
-					})
-				}),
-				entries: List()
+				pair: List.of('Trainspotting', '28 Days Later'),
+				tally: Map({
+					'Trainspotting': 3,
+					'28 Days Later': 2 
+				})
 			});
-			const nextState = vote(state, 'Trainspotting');
+			const nextState  = vote(state, 'Trainspotting');
 			
 			expect(nextState).to.equal(Map({
-				vote: Map({
-					pair: List.of('Trainspotting', '28 Days Later'),
-					tally: Map({
-						'Trainspotting': 4,
-						'28 Days Later': 2
-					})
-				}),
-				entries: List()
+				pair: List.of('Trainspotting', '28 Days Later'),
+				tally: Map({
+					'Trainspotting': 4,
+					'28 Days Later': 2
+				})
 			}));
-			
 		});
-	});
-	
-	
-	
+	});	
 	
 	
 });
+
+
 
 
 
