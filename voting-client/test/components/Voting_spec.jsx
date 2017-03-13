07b6d1fa470d88ea70b6b1bbe597d37d9266d9ba
unknown <dozh@hw296.corp.ecc.kz>
2017-03-13 18:26:14 +0600

    pure rendering

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -5,6 +5,7 @@ import {
 	scryRenderedDOMComponentsWithTag,
 	Simulate
 } from 'react-addons-test-utils';
+import {List} from 'immutable';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
 
@@ -71,4 +72,44 @@ describe('Voting', () => {
 	  expect(winner.textContent).to.contain('Trainspotting');
 	});
 	
+	it('otrisovyvaet kak chistyi komponent', () => {
+	  const pair = ['Trainspotting', '28 Days Later'];
+	  const container = document.createElement('div');
+	  let component = ReactDOM.render(
+		<Voting pair={pair} />,
+		container
+	  );
+
+	  let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+	  expect(firstButton.textContent).to.equal('Trainspotting');
+
+	  pair[0] = 'Sunshine';
+	  component = ReactDOM.render(
+		<Voting pair={pair} />,
+		container
+	  );
+	  firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+	  expect(firstButton.textContent).to.equal('Trainspotting');
+	});
+	
+	it('obnovliaet DOM pri izmenenii svoistv', () => {
+		const pair = List.of('Trainspotting', '28 Days Later');
+		const container = document.createElement('div');
+		let component = ReactDOM.render(
+			<Voting pair={pair} />,
+			container
+		  );
+		let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+		expect(firstButton.textContent).to.equal('Trainspotting');
+		
+		const newPair = pair.set(0, 'Sunshine');
+		component = ReactDOM.render(
+		  <Voting pair={newPair} />,
+		  container
+		);
+		firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+		expect(firstButton.textContent).to.equal('Sunshine');
+	});
+	
 });
+
